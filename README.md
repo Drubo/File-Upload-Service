c46172b7ce7b94d17c48ba226c11ad184b50d570
Tariqul Islam <tareq@webkutir.net>
2012-02-11 18:19:59 +0600

    Xhr Polling with Progress Bar Complete....

--- README.md
+++ README.md
@@ -10,4 +10,24 @@ This server will handle the following types of Upload:
 3.  Xhr long Polling
 4.  iFrame Support.
 
-Currently only the first two types are supported. We are working on the support the other two types.
+
+**Currently we can support the followings:**
+
+1.  WebSocket
+2.  Flash based WebSocket (Require Flash Player 10.0+)
+3.  Xhr long Polling with progress bar
+
+
+**Currently Supported Browsers:**
+
+1.  Chrome 3 - 16
+
+2.  FireFox 3.5, FireFox 4.0 - support for WebSockets disabled. To enable it [see here](http://techdows.com/2010/12/turn-on-websockets-in-firefox-4.html), FireFox 6.0 - prefixed `MozWebSocket`, FireFox 7.0 + 8.0 + 9.0 + 10.0 - prefixed: `MozWebSocket`, FireFox 11
+
+3.  Safari 1.2+, 4, 5.0.0, 5.0.2, 5.1
+
+4.  Opera 7.6+, Opera 11 - with support disabled. To enable it [see here](http://techdows.com/2010/12/enable-websockets-in-opera-11.html) (For WebSocket).
+
+5.  IE 5, IE 5.5, IE 6, IE 7.x - IE 9.x, IE 10 - via Silverlight extension, IE 10 (from Windows 8 developer preview)
+
+6.  Netscape 7+
--- js/xhr_polling_form_data.js
+++ js/xhr_polling_form_data.js
@@ -1,55 +1,7 @@
-function xhr_upload(){
-	var formData = new FormData();
-	try{
-		var xhr = new XMLHttpRequest();
-	}catch(e){
-		try{
-			var xhr = new ActiveXObject("Microsoft.XMLHttp"); //IE6 or Older
-		}catch(e){
-			
-		}
-	}
-	
-	var onProgress = function(e) {
-	  //var ary = e.target.responseText.split("\n");
-	  //ary.forEach(function(data){
-		//  var pcs = data.split("||");
-		  //var percentComplete = (pcs[0].split(":")[1]/pcs[1].split(":")[1])*100;
-		  //console.log(percentComplete);
-	  //});
-		console.log(e.target.responseText);
-	};
-
-	var onError = function(e) {
-	  // something went wrong with upload
-	  //console.log(e);
-	  //console.log('Error');
-	};
-	
-	var onLoad = function(e){
-		//console.log(e);
-	};
-	
-	var onReady = function(e){
-		//console.log(e);
-	};
-	
-	formData.append('file_name_input', file_name_input[0].files[0]);
-	xhr.addEventListener('error', onError, false);
-	xhr.addEventListener('progress', onProgress, false);
-	xhr.addEventListener('load', onLoad, false);
-	xhr.addEventListener('readystatechange', onReady, false);
-	xhr.open('post', 'http://hostFromServer/upload', true);
-	xhr.send(formData);
-}
-
-function makeObject(obj){
-	return eval("(" + obj + ")");
-}
-
-function xhr_cancel(){
-	xhr.cancel(formData);
-}
+var formData;
+var xhr;
+var start_time;
+var start_file_index;
 
 var fc = $('.file_uploader');
 var file_progress_bar = fc.find('.file_progress_bar');
@@ -66,10 +18,92 @@ var file_reupload_button = fc.find('.file_reupload_button');
 var file_name_input = fc.find('.file_name_input');
 var file_buttons = fc.find(':button');
 
-//alert(file_name_input[0].files);
+var allowed_types = typesFromServer;
 
 reset_upload();
 bind_events();
+
+function xhr_upload(){
+	start_time = Date.now();
+	start_file_index = 0;
+	
+	formData = new FormData();
+	try{
+		xhr = new XMLHttpRequest();
+	}catch(e){
+		try{
+			xhr = new ActiveXObject("Msxml2.XMLHTTP.6.0"); //IE6 or Older
+		}catch(e){
+			try{
+				xhr =new ActiveXObject("Msxml2.XMLHTTP.3.0");
+			}catch(e){
+				try{
+					xhr = new ActiveXObject("Microsoft.XMLHttp");
+				}catch(e){
+					
+				}
+			}
+		}
+	}
+
+	var onProgress = function(e){
+		var progress = Math.floor(100 * e.loaded / e.total);
+		calculate_eta(e.loaded, e.total);
+	    file_progress_bar.progressbar({value: progress});
+	};
+	
+	var onError = function(e) {
+		file_information.html(e.target.responseText);
+	};
+	
+	var onLoad = function(e){
+		if(e.target.responseText=='Upload Complete'){
+			reset_upload();
+			file_information.html(e.target.responseText);
+			file_progress_bar.progressbar({value: 100});
+			xhr = null;
+			formData = null;
+		}
+	};
+	
+	var onLoadStart = function(e){
+		file_information.html('Uploading '+file_name_input[0].files[0].name+"...");
+	};
+	
+	formData.append('file_name_input', file_name_input[0].files[0]);
+	xhr.addEventListener('error', onError, false);
+	xhr.addEventListener('load', onLoad, false);
+	xhr.upload.addEventListener('progress', onProgress, false);
+	xhr.addEventListener('loadstart', onLoadStart, false);
+	xhr.open('post', 'http://hostFromServer/upload', true);
+	xhr.send(formData);
+}
+
+function xhr_cancel(){
+	try{
+		xhr.abort();
+		reset_upload();
+	}catch(e){
+		
+	}
+}
+
+function calculate_eta(file_index, file_size){
+  var delta_ms = Date.now() - start_time;
+  var rate = (file_index- start_file_index) / delta_ms; 
+  var remaining_ms = (file_size - file_index) / rate; 
+  var delta_hr = parseInt(Math.floor(remaining_ms/3600000));
+  remaining_ms -= delta_hr*3600000;
+  var delta_min = parseInt(Math.floor(remaining_ms/60000));
+  remaining_ms -= delta_min*60000;
+  var delta_sec = parseInt(Math.floor(remaining_ms/1000));
+  if (delta_sec>10) delta_sec = parseInt(Math.floor(delta_sec/10)*10);
+  var eta = "";
+  if (delta_sec>0) eta = delta_sec + " secs";
+  if (delta_min>0) eta = delta_min + " mins";
+  if (delta_hr>0) eta = delta_hr + " hours";
+  if (delta_ms>5000) file_progress.html(eta);  
+}
 
 function reset_upload(){
   file_buttons.hide();
@@ -82,30 +116,64 @@ function reset_upload(){
   file_progress_bar.progressbar({value:0});
 }
 
+function validate(){
+	if(allowedTypes!='undefined'){
+		allowed_types = allowedTypes;
+	}
+
+	var parts = file_name_input[0].files[0].name.split('.');
+	var ext = parts[parts.length-1];
+	  
+	//Check for valid file type
+	var ary = allowed_types.split('|');
+	var test = ext in oc(ary);
+	if (!test) {
+		reset_upload();
+		file_information.html("You are not allowed to upload this type of file.");
+		return;
+	}
+	
+	if(file_name_input[0].files[0].size > maxFileSize){
+		reset_upload();
+		file_information.html("This file is too large to upload...");
+		return;
+	}
+}
+
+function oc(a){
+  var o = {};
+  for(var i=0;i<a.length;i++)
+  {
+    o[a[i]]='';
+  }
+  return o;
+};
+
 function bind_events(){
-  // Start upload button
-   file_upload_button.bind('click', {uploader: this}, function (e) {
+   // Start upload button
+   file_upload_button.bind('click', function (e) {
     file_buttons.hide();
     file_cancel_button.show();
     xhr_upload();
     return false; 
   });
    
-  file_browse_button.bind('click', {uploader: this}, function (e) { 
+  file_browse_button.bind('click', function (e) { 
 	  file_name_input.click();
 	  return false;
   });
   
-  file_name_input.bind('change', {uploader: this}, function(e) {
+  file_name_input.bind('change', function(e) {
       file_information.html(file_name_input[0].files[0].name);
       file_progress.html('');
       file_buttons.hide();
       file_browse_button.show();
       file_upload_button.show();
       file_progress_bar.progressbar({value:0});
+      validate();
     });
   
-   file_cancel_button.bind('click', {uploader: this}, function(e){
+   file_cancel_button.bind('click', function(e){
 	  xhr_cancel();
 	  return false;
    });
